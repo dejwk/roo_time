10edec2d7748feab10c66ab21816d33555e24e58
Dawid Kurzyniec <dawid.kurzyniec@gmail.com>
2026-08-22 00:48:06 +0200

    Added the possibility to run the Esp32NtpTime under the emulator.

--- examples/Esp32NtpTime/Esp32NtpTime.ino
+++ examples/Esp32NtpTime/Esp32NtpTime.ino
@@ -1,16 +1,50 @@
 // This example is for ESP32 boards, demonstrating how to get
 // time from an NTP server and print it to Serial in a human-readable format.
 
+// -- EMULATOR SETUP
+
+#ifdef ROO_TESTING
+#include <memory>
+
+#include "roo_testing/microcontrollers/esp32/fake_esp32.h"
+#include "roo_testing/transducers/wifi/wifi.h"
+
+// Make the standard WiFi API connect to an in-process access point. Time is
+// supplied by roo_testing's ESP32 time shim, which mirrors the host clock.
+struct Emulator {
+  roo_testing_transducers::wifi::Environment wifi;
+
+  Emulator() {
+    auto ap = std::make_unique<roo_testing_transducers::wifi::AccessPoint>(
+        roo_testing_transducers::wifi::MacAddress(1, 1, 1, 1, 1, 1),
+        "testwifi");
+    ap->setAuthMode(roo_testing_transducers::wifi::AUTH_WEP);
+    ap->setPasswd("pwd");
+    wifi.addAccessPoint(std::move(ap));
+    FakeEsp32().setWifiEnvironment(wifi);
+  }
+} emulator;
+
+const char* wifi_ssid = "testwifi";
+const char* wifi_password = "pwd";
+
+#else
+
+const char* wifi_ssid = "<enter your SSID>";
+const char* wifi_password = "<enter your password>";
+
+#endif
+
+// -- EXAMPLE STARTS HERE
+
 #include <Arduino.h>
+
 #include <WiFi.h>
 
 #include "roo_time.h"
 #include "time.h"
 
 using namespace roo_time;
-
-const char* wifi_ssid = "<enter your SSID>";
-const char* wifi_password = "<enter your password>";
 
 const char* ntpServer = "pool.ntp.org";
 
@@ -21,6 +55,8 @@ SystemClock my_clock;
 
 void setup() {
   Serial.begin(9600);
+  Serial.printf("Connecting to %s", wifi_ssid);
+  WiFi.mode(WIFI_STA);
   WiFi.begin(wifi_ssid, wifi_password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
@@ -28,6 +64,7 @@ void setup() {
   }
   Serial.println("WiFi connected.");
   configTime(0, 0, ntpServer);
+  Serial.printf("NTP time configured from %s.\n", ntpServer);
 }
 
 void loop() {
